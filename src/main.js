74972d99890cd4b8ff5c21dcf980f407cce1200d
alnabeel-acs <111437266+alnabeel-acs@users.noreply.github.com>
2024-06-12 11:08:24 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ export default async ({ req, res, log, error }) => {
   if (req.method === 'GET') {
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
+    console.log("jbxjahb");
     return res.send('Hello, World!');
   }
 
