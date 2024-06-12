66dde109d29663c34b3436732e8c94f6890d01e9
alnabeel-acs <111437266+alnabeel-acs@users.noreply.github.com>
2024-06-12 11:45:10 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import { Client } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   // Initialize the Appwrite client
   const client = new Client();
-  client
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject("66686b30000336ef6b28") // Your project ID
-    .setKey(process.env.APPWRITE_API_KEY); // Your API key
+  // client
+  //   .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+  //   .setProject("66686b30000336ef6b28") // Your project ID
+  //   .setKey(process.env.APPWRITE_API_KEY); // Your API key
 
   // Log a message to the console
   log('Hello, Logs!');
